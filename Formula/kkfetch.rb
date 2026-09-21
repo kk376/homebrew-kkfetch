1b39280d389d5209b106d47c7779db013ffe7c65
Kushagra Kumar <kkushagra86@gmail.com>
2026-09-21 06:20:17 +0530

    feat(formula): bump kkfetch to v0.15.0

--- Formula/kkfetch.rb
+++ Formula/kkfetch.rb
@@ -2,11 +2,11 @@ class Kkfetch < Formula
   desc "Fast, lightweight Linux, macOS, and Windows system information fetch tool written in Rust"
   homepage "https://github.com/kk376/kkfetch"
   license "MIT"
-  version "0.14.5"
+  version "0.15.0"
 
   on_macos do
-    url "https://github.com/kk376/kkfetch/archive/refs/tags/v0.14.5.tar.gz"
-    sha256 "b6acbd3ccbd3fafb50639da55e8421a6f1b8faf76733d2aa37ed15e5abc3ac2a"
+    url "https://github.com/kk376/kkfetch/archive/refs/tags/v0.15.0.tar.gz"
+    sha256 "866d8aba159615f988ba66f169f2153c3c3819949ced9c47df2ad8d1c3303259"
     depends_on "rust" => :build
 
     def install
@@ -19,8 +19,8 @@ class Kkfetch < Formula
   end
 
   on_linux do
-    url "https://github.com/kk376/kkfetch/releases/download/v0.14.5/kkfetch-0.14.5-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "b0c992dbc8f97d3f9e929324dc59d46dc1aee07a9ee2dbc16a0b6aef9ae0f5a1"
+    url "https://github.com/kk376/kkfetch/releases/download/v0.15.0/kkfetch-0.15.0-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "30979191565e15787c4e4176b6a1a14578f19d5c70ea0b81a84cf00f665d28bc"
 
     def install
       bin.install "kkfetch"
@@ -32,7 +32,7 @@ class Kkfetch < Formula
   end
 
   test do
-    assert_match "kkfetch 0.14.5", shell_output("#{bin}/kkfetch --version")
+    assert_match "kkfetch 0.15.0", shell_output("#{bin}/kkfetch --version")
   end
 end
 
